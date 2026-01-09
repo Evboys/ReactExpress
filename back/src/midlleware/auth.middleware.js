68f4f8ff6da6c53dfe9f6c8893073acd6a45ec95
Evan Boyer <evan.boyer@chausson.fr>
2026-01-09 14:01:29 +0100

    Route Info Perso et middleware

--- back/src/midlleware/auth.middleware.js
+++ back/src/midlleware/auth.middleware.js
@@ -1,17 +1,23 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
-    const auth = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!auth) return res.status(401).json({ message: "Token manquant" });
+    if (!authHeader) {
+        return res.status(401).json({ message: "Token manquant" });
+    }
 
-    const token = auth.split(" ")[1];
+    const [type, token] = authHeader.split(" ");
+
+    if (type !== "Bearer" || !token) {
+        return res.status(401).json({ message: "Format du token invalide" });
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = decoded; // { id, username, iat, exp }
         next();
-    } catch {
-        res.status(401).json({ message: "Token invalide ou expiré" });
+    } catch (error) {
+        return res.status(401).json({ message: "Token invalide ou expiré" });
     }
 };
--- back/src/routes/auth.routes.js
+++ back/src/routes/auth.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { authMiddleware } from "../midlleware/auth.middleware.js";
 import { register, login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/me", authMiddleware, async (req, res) => {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+})
 
 export default router;
