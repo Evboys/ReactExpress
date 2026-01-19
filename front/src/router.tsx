e53c5fca5387afa7f050539412082e8ea636d677
Evboys <128623554+Evboys@users.noreply.github.com>
2026-01-19 04:30:20 +0100

    Test de l'url en ligne au moment du deploy

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -5,4 +5,6 @@ import GameDetailPage from "./pages/GameDetailPage";
 export const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
     { path: "/games/:id", element: <GameDetailPage /> }
-]);
+], {
+    basename: "/ReactExpress/"
+});
