381cc8b9117f065461313e7f345fcfda49f89ade
Evboys <128623554+Evboys@users.noreply.github.com>
2026-01-19 05:54:36 +0100

    ++

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -18,8 +18,6 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => (
   </AuthProvider>
 );
 
-const basename = import.meta.env.DEV ? "" : "/ReactExpress";
-
 export const router = createHashRouter(
   [
     {
@@ -59,7 +57,4 @@ export const router = createHashRouter(
       element: <LayoutWrapper><NotFoundPage /></LayoutWrapper>,
     },
   ],
-  {
-    basename: basename,
-  }
 );
